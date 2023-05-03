2ff8cd92aad8e999cea8699e2ac23e1186ba7d0f
Paweł Mendroch <pawelmendroch7@gmail.com>
2023-05-03 14:49:07 +0200

    Fix the new pricing method

--- trading_bot/PriceCalculator.py
+++ trading_bot/PriceCalculator.py
@@ -15,17 +15,21 @@ class CurrencyPrice:
         self,
         name: str,
         mode: Literal["sell", "buy"],
-        price: float,
+        left_side_price: int,
+        right_side_price: int,
     ) -> None:
         self.name = name
         self.mode: Literal["sell", "buy"] = mode
-        self.price = price
+        self.left_side_price = left_side_price
+        self.right_side_price = right_side_price
 
     def get_note(self, exchange_name: str):
         if self.mode == "buy":
-            return f"~price {self.price} {exchange_name}"
+            return (
+                f"~price {self.left_side_price}/{self.right_side_price} {exchange_name}"
+            )
         else:
-            return f"~price {self.price} chaos"
+            return f"~price {self.left_side_price}/{self.right_side_price} chaos"
 
 
 class PriceCalculator:
@@ -72,27 +76,34 @@ class PriceCalculator:
                     raise Exception("Currency: {currency} doesn't have enough listings")
 
                 listings = list(res["result"].values())
+                listings_values = listings[5:20]
                 listings_values = list(
                     map(
-                        lambda x: round(
+                        lambda x: (
+                            x["listing"]["offers"][0]["exchange"]["amount"],
+                            x["listing"]["offers"][0]["item"]["amount"],
                             x["listing"]["offers"][0]["exchange"]["amount"]
                             / x["listing"]["offers"][0]["item"]["amount"],
-                            4,
                         ),
-                        listings,
+                        listings_values,
                     )
                 )
-                listings_values = listings_values[5:20]
+
+                listings_median = median(list(map(lambda x: x[2], listings_values)))
+
+                chosen_listing = list(
+                    filter(lambda x: x[2] == listings_median, listings_values)
+                )[0]
 
                 if stash_tab.mode == "buy":
                     self.buy_currency_config[currency] = CurrencyPrice(
-                        currency, stash_tab.mode, median(listings_values)
+                        currency, stash_tab.mode, chosen_listing[0], chosen_listing[1]
                     )
                 else:
                     self.sell_currency_config[currency] = CurrencyPrice(
-                        currency, stash_tab.mode, median(listings_values)
+                        currency, stash_tab.mode, chosen_listing[0], chosen_listing[1]
                     )
-                pyautogui.sleep(3)
+                pyautogui.sleep(1)
 
     def get_buy_note(self, currency_name: str, exchange_name: str):
         return self.buy_currency_config[currency_name].get_note(exchange_name)
